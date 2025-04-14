67aa505438289b22c8540456b8de079aa693c42d
Maxim Lucier <luciermaxim@gmail.com>
2025-04-14 19:55:27 -0400

    4-5 A4 (had to use snippet of code online to fix a reacurring error)

---
+++ src/app/services/shipment.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Shipment} from '../models/shipment.model';
+import { MOCK_SHIPMENTS} from '../data/mock-content';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShipmentService {
+  private shipments = MOCK_SHIPMENTS;
+
+  getShipments(): Shipment[] {
+    return this.shipments;
+  }
+
+  addShipments(shipment: Shipment): void {
+    this.shipments.push(shipment);
+  }
+  updateShipment(id: number, updatedShipment: Shipment): void {
+  const index = this.shipments.findIndex(s => s.id === id);
+  if (index !== -1) {
+    this.shipments[index] = updatedShipment
+  }
+  }
+
+  deleteShipment(id: number): void {
+    this.shipments = this.shipments.filter(s => s.id !== id);
+  }
+}
