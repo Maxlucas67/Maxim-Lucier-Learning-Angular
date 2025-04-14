99af16007d1c872215ed9cf9e6845fdd1cb9b3ea
Maxim Lucier <luciermaxim@gmail.com>
2025-04-14 15:51:11 -0400

    Created new data type with 5 properties.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,3 +12,11 @@ export class AppComponent {
   name = 'Maxim Lucier'
   studentNum = '0844425'
 }
+
+export interface login {
+  name: string;
+  phoneNum?: string;
+  email: string;
+  age: number;
+  shippingAdress: string;
+}
