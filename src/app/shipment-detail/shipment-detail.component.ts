607945a0b1be59c1e9fda9aa841b5a3440502516
Maxim Lucier <luciermaxim@gmail.com>
2025-04-15 02:33:51 -0400

    3-5 A5 and added empty details page to test the nav bar

---
+++ src/app/shipment-detail/shipment-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shipment-detail',
+  imports: [],
+  templateUrl: './shipment-detail.component.html',
+  styleUrl: './shipment-detail.component.css'
+})
+export class ShipmentDetailComponent {
+
+}
